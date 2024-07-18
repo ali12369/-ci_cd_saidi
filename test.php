79743e853072017b6babfd61d530b935c79b7582
ali12369 <aliisaidi2001@gmail.com>
2024-07-18 01:45:21 +0100

    first push

---
+++ test.php
@@ -0,0 +1,17 @@
+
+<?php
+// tests/AdditionTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class AdditionTest extends TestCase
+{
+    public function testAdd()
+    {
+        require 'index.php';
+        $this->assertEquals(3, add(1, 2));
+        $this->assertEquals(5, add(2, 3));
+        $this->assertEquals(0, add(-1, 1));
+    }
+}
+?>
